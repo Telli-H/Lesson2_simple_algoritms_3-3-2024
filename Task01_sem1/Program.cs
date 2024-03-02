a26147ac25d81bb6b24c22476cfc014892e61969
Telli <tellihamzayeva@gmail.com>
2024-03-02 23:37:56 +0400

    modified Program.cs

--- Task01_sem1/Program.cs
+++ Task01_sem1/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+using System.Threading.Channels;
+
+Console.WriteLine("Input 1 number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input 2 number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+string result = num1 == num2*num2 ? "yes":"no";
+Console.WriteLine($"a={num1}, b={num2} => {result}");
+
+
