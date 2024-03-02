896598306f2484d219114207d02e813e69631651
Telli <tellihamzayeva@gmail.com>
2024-03-03 01:31:37 +0400

    solved Task03

--- Task03_sem1/Program.cs
+++ Task03_sem1/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+Console.WriteLine("input 3-digit number");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+int numCheck = num;
+if (num<0) numCheck=-num;
+
+if ( numCheck>=100 && numCheck <= 999 )
+{
+int digitsSum= numCheck/100 + numCheck % 10;
+Console.WriteLine($"{num} = {digitsSum}");
+}
+else 
+{
+    Console.WriteLine("incorect input");
+}
