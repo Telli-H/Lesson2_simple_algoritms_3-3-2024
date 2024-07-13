8df28fc338ec465c74471a3296a659e42266551e
Telli <tellihamzayeva@gmail.com>
2024-07-14 01:55:43 +0400

    Task3

--- HW_6/Task_2/Program.cs
+++ HW_6/Task_2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+       
+        string originalString = "This Is A Test String WITH Mixed Case LETTERS";
+        
+        
+        string lowerCaseString = originalString.ToLower();
+        
+        
+        Console.WriteLine(lowerCaseString);
+    }
+}
