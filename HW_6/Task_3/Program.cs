99639869b1ae628eb16963e67e539a3eb1d4fc48
Telli <tellihamzayeva@gmail.com>
2024-07-14 02:26:46 +0400

    Task3

---
+++ HW_6/Task_3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+      
+        string originalString = "level";
+        
+        
+        string processedString = originalString.Replace(" ", "").ToLower();
+        
+        
+        bool isPalindrome = IsPalindrome(processedString);
+        
+        
+        if (isPalindrome)
+        {
+            Console.WriteLine("it is a palindrom");
+        }
+        else
+        {
+            Console.WriteLine("it is NOT a palindrom");
+        }
+    }
+
+    static bool IsPalindrome(string str)
+    {
+        int length = str.Length;
+        for (int i = 0; i < length / 2; i++)
+        {
+            if (str[i] != str[length - i - 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
