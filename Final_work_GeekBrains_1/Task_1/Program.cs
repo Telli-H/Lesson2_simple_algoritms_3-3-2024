af85f41e502d13cf35d507fe8970395b9ad55aa3
Telli <tellihamzayeva@gmail.com>
2024-07-16 03:28:49 +0400

    task_1

---
+++ Final_work_GeekBrains_1/Task_1/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк 
+//формирует новый массив из строк, длина которых меньше, 
+//либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+//либо задать на старте выполнения алгоритма. При решении не рекомендуется 
+//пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+string[] inputArr = { "Hello", "2", "world", ":-)" };
+
+string[] inputArr = { "Hello", "2", "world", ":-)" };
+  int resultIndex = 0;
+
+   foreach (string str in inputArr)
+    {
+           
+            if (str.Length <= 3)
+            {
+          
+                resultArray[resultIndex] = str;
+                resultIndex++;
+            }
+        }
+
+string[] finalResult = new string[resultIndex];
+        Array.Copy(resultArray, finalResult, resultIndex)
+
+         Console.WriteLine("initial array:");
+        PrintArray(inputArray);
+
+         Console.WriteLine("New array with strings, which length less than 3 symbols:");
+        PrintArray(finalResult);
+
+        static void PrintArray(string[] arr)
+    {
+        Console.Write("[");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write($"\"{arr[i]}\"");
+            if (i < arr.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine("]");
+    }
